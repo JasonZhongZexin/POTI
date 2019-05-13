0bb71b06a60af81f39bc10390e435c70308b49d2
JasonZhongZexin <jason.zhongzexin@gmail.com>
2019-05-13 20:53:22 +1000

    Update process_email.php
    empty cart and send out email

--- process_email.php
+++ process_email.php
@@ -1,8 +1,13 @@
 <?php
+    session_start();
     ini_set("display_errors","On");
     error_reporting(E_ALL);
+    session_destroy();
 ?>
 <html>
+    <head>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    </head>
     <body>
         <p>
         <?php
@@ -10,54 +15,60 @@
         $email=$_REQUEST['email'];
 
     function send_email($toEmail,$first_name)
-{
-    require_once('./PHPMailer-5.2.27/class.phpmailer.php');
-    require_once('./PHPMailer-5.2.27/class.smtp.php');
-    $mail = new PHPMailer();
-    $mail->IsSMTP(); 
-    $mail->Host = "smtp.gmail.com";            //smtp host
-    $mail->Port = 465;                        //email port 
-    $mail->SMTPAuth   = true;                //allow smtp
-    $mail->CharSet  = "UTF-8";               //encoding format
-    $mail->Encoding = "base64";              //encoding format
-    $mail->IsHTML(true);                     //support html
+    {
+    // require_once('./PHPMailer-5.2.27/class.phpmailer.php');
+    // require_once('./PHPMailer-5.2.27/class.smtp.php');
+    // $mail = new PHPMailer();
+    // $mail->IsSMTP(); 
+    // $mail->Host = "smtp.gmail.com";            //smtp host
+    // $mail->Port = 465;                        //email port 
+    // $mail->SMTPAuth   = true;                //allow smtp
+    // $mail->CharSet  = "UTF-8";               //encoding format
+    // $mail->Encoding = "base64";              //encoding format
+    // $mail->IsHTML(true);                     //support html
  
      
-    $mail->Username = "sep.project.database@gmail.com";      //email username
-    $mail->Password = "Zzx13411989327";      //email password
-    $mail->From     = "sep.project.database@gmail.com";      //from
-    $mail->SMTPSecure = 'ssl'; 
+    // $mail->Username = "";      //email username
+    // $mail->Password = "";      //email password
+    // $mail->From     = "";      //from
+    // $mail->SMTPSecure = 'ssl'; 
      
-    $mail->FromName = "POIT GROCERY STORE";             //from
-    $mail->AddAddress($toEmail);             //to
+    // $mail->FromName = "POIT GROCERY STORE";             //from
+    // $mail->AddAddress($toEmail);             //to
  
     // email subject
     $subject = 'NEW ORDER INFO'; 
-    $mail->Subject = $subject; 
+    // $mail->Subject = $subject; 
  
     // email content
     $content = "Dear " . $first_name . ", <br>Thanks for shopping with us. Your order has been processed. Here is the detail of your order.<br>";
-    $mail->Body = $content;
+    // $mail->Body = $content;
+    $headers = "POIT GROCERY STORE";
+    mail($toEmail,$subject,$content,$headers);
  
     //send eamil
-    if(!$mail->Send()) {
-          return false;
-    }else{
-          return true;
+    // if(!$mail->Send()) {
+    //       return false;
+    // }else{
+    //       return true;
+    // }
     }
-}
 
-        if(($first_name!=NULL||$first_name!="")&& ($email!=NULL||$email!="")){
-            // sendEmail($email,$first_name);
-            $status = send_email($email, $first_name);
-            // if($status){
-            //     echo 'email has sent';
-            // }else{
-            //     echo 'fail to send the email';
-            // }
-            echo "<br> Thanks for shopping with us. Your order has been processed and a confirmation email has been send to $email";
-        }
-        ?>
-        </p>
-    </body>
+    if(($first_name!=NULL||$first_name!="")&& ($email!=NULL||$email!="")){
+        // sendEmail($email,$first_name);
+        send_email($email, $first_name);
+        // if($status){
+        //     echo 'email has sent';
+        // }else{
+        //     echo 'fail to send the email';
+        // }
+        echo "<br> <h2>Thanks for shopping with us. Your order has been processed and a confirmation email has been send to $email<h2>";
+    }
+?>
+</p>
+<form action="shoppingCart.php?clear=true" target="shoppingCart_iframe"></form>
+        <script>
+            $("form").submit();
+        </script>
+</body>
 </html>
